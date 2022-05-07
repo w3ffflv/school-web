06f8d022b25b90aa5f36917beda89a852ce60d72
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-07 04:20:35 +0300

    setting changes

--- skola/settings.py
+++ skola/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static",   'static/',]
+STATICFILES_DIRS = [BASE_DIR / "static",   'static/']
 
 
 # Default primary key field type
