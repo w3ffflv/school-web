5e608bdc4f123ba7b2bd8a7b34ff285443002e60
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-07 04:26:27 +0300

    added STATIC root

--- skola/settings.py
+++ skola/settings.py
@@ -111,6 +111,8 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
@@ -118,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [BASE_DIR / "static",   'static/']
 
 
