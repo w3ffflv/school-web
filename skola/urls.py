9051210c2db2d91be089539043096a1acc54a100
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-10 06:59:35 +0300

    q

--- skola/urls.py
+++ skola/urls.py
@@ -19,14 +19,12 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 from skola import settings
 
 
 from . import views
-
-
 
 urlpatterns = [
      path('admin/', admin.site.urls),
@@ -38,6 +36,4 @@ urlpatterns = [
      # Django Auth
      path('accounts/login', auth_views.LoginView.as_view(template_name ="accounts/login.html"), name='login'),
      path('accounts/logout', auth_views.LogoutView.as_view(), name="logout")
-] 
-
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
