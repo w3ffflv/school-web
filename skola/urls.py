245ef618c7070023f1471ee0d31e4949e45df9b8
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-10 06:30:49 +0300

    1

--- skola/urls.py
+++ skola/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 
+from skola import settings
+
 
 from . import views
 
