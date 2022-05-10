1a7915019f856f79ca5d0c96bbe72776bf588f15
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-10 13:36:21 +0300

    url update

--- skola/urls.py
+++ skola/urls.py
@@ -17,6 +17,8 @@ from re import template
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 from . import views
 
@@ -31,3 +33,5 @@ urlpatterns = [
      path('accounts/login', auth_views.LoginView.as_view(template_name ="accounts/login.html"), name='login'),
      path('accounts/logout', auth_views.LogoutView.as_view(), name="logout")
 ]
+urlpatterns += staticfiles_urlpatterns()
+
