02fa63fe463d4d23320ca96d6a5355dc061899b9
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-10 06:21:24 +0300

    q

--- skola/settings.py
+++ skola/settings.py
@@ -121,10 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),]
+STATIC_ROOT = os.path.join(BASE_DIR,'static/') 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 
 # Default primary key field type
