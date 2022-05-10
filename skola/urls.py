47a8afd5b46102ace25e4c5b3da49bada332150d
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-10 07:48:30 +0300

    1

--- skola/urls.py
+++ skola/urls.py
@@ -37,7 +37,3 @@ urlpatterns = [
      path('accounts/logout', auth_views.LogoutView.as_view(), name="logout")
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT)
