44adda4a54177a7126e12b442ebb1914c2b44506
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-11 20:31:41 +0300

    q

--- skola/settings.py
+++ skola/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'skola.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_DIR, "templates")],
+        'DIRS': [os.path.join(PROJECT_DIR, "skola/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
