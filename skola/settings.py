23dea406597a07bcb12ca985537ba634047de758
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-11 18:51:41 +0300

    1

--- skola/settings.py
+++ skola/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+import pymysql
 
+pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
