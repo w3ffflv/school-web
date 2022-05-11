8ef02203479316710e88dc6ef7a3bec4ad227482
Haralds Glaudans <w3ffflv@gmail.com>
2022-05-11 12:44:22 +0300

    backup

--- skola/urls.py
+++ skola/urls.py
@@ -25,13 +25,13 @@ from . import views
 urlpatterns = [
      path('admin/', admin.site.urls),
      path('', views.index, name="index"),
-     #path('about/', views.about, name="about"),
-     #path('contact/', views.contact, name="contact"),
-     #path('accounts/profile/', views.ProfileView.as_view(), name="profile"),
+     path('about/', views.about, name="about"),
+     path('contact/', views.contact, name="contact"),
+     path('accounts/profile/', views.ProfileView.as_view(), name="profile"),
 
-     # Django Auth
-     #path('accounts/login', auth_views.LoginView.as_view(template_name ="accounts/login.html"), name='login'),
-     #path('accounts/logout', auth_views.LogoutView.as_view(), name="logout")
+    #Django Auth
+     path('accounts/login', auth_views.LoginView.as_view(template_name ="accounts/login.html"), name='login'),
+     path('accounts/logout', auth_views.LogoutView.as_view(), name="logout")
 ]
 urlpatterns += staticfiles_urlpatterns()
 
--- skola/views.py
+++ skola/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.template import loader
-#from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def index(request):
@@ -9,13 +9,13 @@ def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render({}, request))
 
-#def about(request):
-  #  template = loader.get_template('about.html')
- #   return HttpResponse(template.render({}, request))
+def about(request):
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render({}, request))
 
-#def contact(request):
-   # template = loader.get_template('contact.html')
-  #  return HttpResponse(template.render({}, request))
+def contact(request):
+    template = loader.get_template('contact.html')
+    return HttpResponse(template.render({}, request))
 
-#class ProfileView(LoginRequiredMixin,TemplateView):
- #   template_name = 'accounts/profile.html'
+class ProfileView(LoginRequiredMixin,TemplateView):
+    template_name = 'accounts/profile.html'
